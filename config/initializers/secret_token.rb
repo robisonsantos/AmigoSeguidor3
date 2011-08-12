a79970e8ad7fadba83df2daa5ab071650b06b64f
Robison WR Santos <rwrsantos@gmail.com>
2011-08-12 19:24:19 -0300

    Adding project files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Amigoseguidor3::Application.config.secret_token = '279abb193c0e6ce896e3ffd57feab84ca241d92b8c6c60c728bf5b576ed75479f2a8a0aa46439d6e75bad40e34b57ea16cb7142d5a9e6b3538447b49619fa246'
