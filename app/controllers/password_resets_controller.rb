9ccc57b8a19b6e616982f95941df99eeea323344
Robison WR Santos <rwrsantos@gmail.com>
2011-08-23 19:29:54 -0300

    adding missing files

---
+++ app/controllers/password_resets_controller.rb
@@ -0,0 +1,44 @@
+class PasswordResetsController < ApplicationController
+#  before_filter :require_no_user
+  before_filter :load_user_using_perishable_token, :only => [ :edit, :update ]
+
+  def new
+  end
+
+  def create
+    @user = User.find_by_email(params[:email])
+    if @user
+      Notifier.deliver_password_reset_instructions(@user)
+      flash[:notice] = "Instructions to reset your password have been emailed to you"
+      redirect_to root_path
+    else
+      flash.now[:error] = "No user was found with email address #{params[:email]}"
+      redirect_to :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @user.password = params[:password]
+    @user.password_confirmation = params[:password]
+    if @user.save
+      flash[:success] = "Your password was successfully updated"
+      redirect_to @user
+    else
+      redirect_to :edit
+    end
+  end
+
+
+  private
+
+  def load_user_using_perishable_token
+    @user = User.find_using_perishable_token(params[:id])
+    unless @user
+      flash[:error] = "We're sorry, but we could not locate your account"
+      redirect_to root_url
+    end
+  end
+end
---
+++ app/mailers/notifier.rb
@@ -0,0 +1,11 @@
+class Notifier < ActionMailer::Base
+  default :from => "from@example.com"
+  #default_url_options[:host] = "localhost:3000" #TODO: change this for production or heroku tests
+  default_url_options[:host] = "amigoseguidor30.heroku.com"
+
+	def password_reset_instructions(user)
+		@user = user
+		@url = edit_password_reset_url(@user.perishable_token)
+		mail(:to => @user.email, :subject => "Password Reset Instruction")
+  end
+end
